3ea80301a294b087136da30a428d2ade4edc683c
Volodymyr Balashov <76092273+DevilDoctor27@users.noreply.github.com>
2021-07-05 17:21:42 +0900

    Completed?

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const range = document.getElementById("range")
 const price = document.getElementById("price")
+const rangeFill = document.querySelector(".slider__fill")
 
 
 range.addEventListener("input", () => {
     price.textContent = "$" + range.value + ".00"
+    rangeFill.style.width = (range.value * 3.3333) + "%"
 })
 
-console.log(range)
+
+const switchToggle = document.querySelector(".card__switch--item")
+const switchParent = document.querySelector(".card__switch")
+
+switchToggle.addEventListener("input", () => {
+    switchParent.classList.toggle("active")
+})
